dc33d7b352594a41d87b005ee13408c8bcff75d2
yunchan312 <phenomenal312@hufs.ac.kr>
2025-06-24 16:02:15 +0900

    fix: footer height

--- challkathon_lavoz_react/src/Layout/Footer.tsx
+++ challkathon_lavoz_react/src/Layout/Footer.tsx
@@ -1,5 +1,7 @@
 const Footer = () => {
-  return <div className="w-full bg-neutral-400">footer</div>;
+  return (
+    <div className="w-full bg-neutral-200 h-[200px] px-5 py-5">footer</div>
+  );
 };
 
 export default Footer;
