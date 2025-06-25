3c0fe7686e57c7dff42c3f88a494c8962319adc6
yunchan312 <phenomenal312@hufs.ac.kr>
2025-06-25 21:38:16 +0900

    feat: mypage

--- challkathon_lavoz_react/src/Routes/MyPage.tsx
+++ challkathon_lavoz_react/src/Routes/MyPage.tsx
@@ -7,13 +7,36 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Input } from "@/components/ui/input";
 import { BadgeCheckIcon } from "lucide-react";
+import { useState } from "react";
+import {
+  FaBookmark,
+  FaComment,
+  FaHeart,
+  FaRegBookmark,
+  FaRegComment,
+  FaRegHeart,
+} from "react-icons/fa";
 
 const MyPage = () => {
+  const [user, setUser] = useState({
+    userName: "이서연",
+    role: "부모",
+    email: "phenomenal312@naver.com",
+  });
   return (
     <div>
       <div className="text-3xl font-bold py-10">마이페이지</div>
-      <div className="border-2 rounded-xl flex items-center py-5 max-w-[900px] mx-auto justify-center gap-2 mb-5">
+      <div className="border-2 shadow-sm rounded-xl flex items-center py-5 max-w-[900px] mx-auto justify-center gap-2 mb-5">
         <div
           className="size-40 bg-center bg-cover rounded-full"
           style={{
@@ -30,13 +53,28 @@ const MyPage = () => {
           </div>
         </div>
         <div className="self-start mx-2">
-          <Button>내 정보 변경</Button>
+          <Drawer>
+            <DrawerTrigger asChild>
+              <Button>내 정보 변경</Button>
+            </DrawerTrigger>
+            <DrawerContent>
+              <form className="max-w-[900px] w-full mx-auto mb-10 h-[30vh]">
+                <DrawerHeader>
+                  <DrawerTitle>내 정보 변경</DrawerTitle>
+                  <DrawerDescription>내 정보를 변경합니다.</DrawerDescription>
+                </DrawerHeader>
+
+                <div className="w-full flex flex-col gap-5">
+                  <Input defaultValue={user.userName} />
+                  <Input defaultValue={user.email} />
+                  <Input defaultValue={user.userName} />
+                  <Button className="w-full">제출</Button>
+                </div>
+              </form>
+            </DrawerContent>
+          </Drawer>
         </div>
       </div>
-
-      {/* <div className="border-2 rounded-xl py-5 max-w-[900px] mx-auto gap-2 mb-5">
-        
-      </div> */}
     </div>
   );
 };
